91f524ee4fd74ace0e3e54df9747108864fca5e0
masoudrk95 <masoud.rk95@gmail.com>
2022-12-31 14:20:55 +0330

    exception handler for stream error.

--- proxy.js
+++ proxy.js
@@ -1,33 +1,134 @@
 const socks = require('socksv5');
 const { readFileSync } = require('fs');
-const { Client } = require('ssh2');
+const SSH2 = require('ssh2');
 const regedit = require('regedit').promisified
+// const GlobalProxy = require("node-global-proxy").default;
+// const globalTunnel = require('global-tunnel-ng');
 
 const sshServer = {
     host: '3.126.153.90',
     port: 22,
     username: 'ubuntu',
-    privateKey: readFileSync('C:/Users/MRK/Downloads/german-key-openssh.pem', 'utf8'),
+    privateKey: readFileSync('C:/Users/MasoudRK/Downloads/Telegram Desktop/german-key.pem', 'utf8'),
     keepaliveInterval: 50000,
     //   debug: (s) => {console.log(s)} 
-};
+}
 
 const socksServer = {
     host: '127.0.0.1',
     port: 54612
 }
-try {
+ 
+class SSHClient {
 
-    // const conn = new Client();
-    // conn.on('ready', async () => {
-        // console.log('connected to ssh server => ' + sshServer.host + ':' + sshServer.port);
+    config = null;
+    conn = null;
+    isOpen = false;
 
-        regedit.putValue({
-            'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings': {
-                ProxyEnable: { value: 1, type: 'REG_DWORD' }
+    constructor(config){
+        this.config = config;
+        this.isOpen = false
+    }
+
+    async start(){
+        this.conn = await this.connect();
+        this.conn.on('close',async () => {
+            console.log('closed');
+            this.isOpen = false; 
+            this.conn = null;
+
+            try {
+                await this.start();
+                this.isOpen = true; 
+            } catch (error) {  
+                console.log('error', error);
             }
+        });
+        this.conn.on('error', (err) => {
+            console.log('error', err);
+        });
+        this.conn.on('end', () => {
+            console.log('end');
+        }); 
+    }
+
+    connect(){ 
+        return new Promise((resolve, reject)=>{
+            const conn = new SSH2.Client(); 
+
+            conn.on('ready', async () => {
+                // this.isOpen = true;
+                console.log('connected to ssh server => ' + this.config.host + ':' + this.config.port);
+                resolve(conn)  
+            }).connect(this.config); 
+        }) 
+    } 
+}
+
+class SSHProxy {
+    constructor(){
+
+    }
+ 
+    async start(){ 
+        await this.enableSystemProxy();
+
+        const sshClient = new SSHClient(sshServer);
+        await sshClient.start();
+    
+        const socksInstance = socks.createServer((info, accept, deny) => {
+            console.log(`request ${info.srcAddr}:${info.srcPort} ===> ${info.dstAddr}:${info.dstPort}`);   
+    
+            sshClient.conn.forwardOut(info.srcAddr, info.srcPort,
+                info.dstAddr,
+                info.dstPort,
+                (err, resultStream) => {
+    
+                    console.log(`transmit ${info.srcAddr}:${info.srcPort} <== ${info.dstAddr}:${info.dstPort}`);   
+    
+                    if (err) { 
+                        console.log('error exception', err);
+                        return deny();
+                        // return;
+                    }
+    
+                    const clientSocket = accept(true);
+                    if (clientSocket) { 
+                        try { 
+                            // resultStream.pipe(clientSocket).on('data', (data)=>{
+                            //     console.log(data);
+                            // })
+                            resultStream
+                                .pipe(clientSocket)
+                                .pipe(resultStream)
+                                     .on('error', () => { 
+                                         console.log(`error in socket write ${info.srcAddr}:${info.srcPort} <== ${info.dstAddr}:${info.dstPort} `);   
+                                     })   
+                                     .on('close', () => { 
+                                        //  console.log(`transmitted.`);   
+                                     });
+                        } catch (ex) {
+                            console.log('exception', ex);
+                        }
+                    } else { 
+    
+                    } 
+            }); 
         })
-        regedit.putValue({
+    
+        socksInstance.listen(socksServer.port, socksServer.host, () => {
+            console.log('SOCKSv5 proxy server started on ' + socksServer.host + ':' + socksServer.port);
+        }).useAuth(socks.auth.None());
+    
+    
+    
+    }
+    
+
+    async enableSystemProxy(){
+        
+        console.log('setting registry keys.');
+        await regedit.putValue({
             'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings': {
                 ProxyServer: {
                     value: 'socks=' + socksServer.host + ':' + socksServer.port, type: 'REG_SZ'
@@ -38,97 +139,36 @@ try {
                 }
             }
         })
-
-        const socksInstance = socks.createServer((info, accept, deny) => {
-            console.log(`request ${info.srcAddr}:${info.srcPort} ===> ${info.dstAddr}:${info.dstPort}`);   
-
-            var conn = new Client();
-            conn.on('ready', function () {
-                conn.forwardOut(info.srcAddr,
-                    info.srcPort,
-                    info.dstAddr,
-                    info.dstPort,
-                    function (err, stream) {
-                        if (err) {
-                            console.log(err);
-                            conn.end();
-                            return deny();
-                        }
-
-                        var clientSocket;
-                        if (clientSocket = accept(true)) {
-                            stream.pipe(clientSocket).pipe(stream).on('close', function () {
-                                conn.end();
-                            });
-                        } else
-                            conn.end();
-                    });
-            }).on('error', function (err) {
-                deny();
-                console.log(err);
-            }).connect(sshServer);
-
-            // conn.forwardOut(info.srcAddr, info.srcPort,
-            //     info.dstAddr,
-            //     info.dstPort,
-            //     (err, resultStream) => {
-
-            //         console.log(`request ${info.srcAddr}:${info.srcPort} ===> ${info.dstAddr}:${info.dstPort}`);   
-
-            //         if (err) { 
-            //             console.log('error exception', err);
-            //             return deny();
-            //             // return;
-            //         }
-
-            //         const clientSocket = accept(true);
-            //         if (clientSocket) { 
-            //             try { 
-            //                 // resultStream.pipe(clientSocket).on('data', (data)=>{
-            //                 //     console.log(data);
-            //                 // })
-            //                 resultStream
-            //                     .pipe(clientSocket)
-            //                     .pipe(resultStream)
-            //                          .on('error', () => { 
-            //                              console.log(`error.`);   
-            //                          })   
-            //                          .on('close', () => { 
-            //                              console.log(`transmitted.`);   
-            //                          });
-            //             } catch (ex) {
-            //                 console.log('exceeeeeeeeeeeeeeeeption', ex);
-            //             }
-            //         } else { 
-
-            //         } 
-            // });
-
+        await regedit.putValue({
+            'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings': {
+                ProxyEnable: { value: 1, type: 'REG_DWORD' }
+            }
         })
 
-        socksInstance
-            .listen(socksServer.port, socksServer.host, () => {
-                console.log('SOCKSv5 proxy server started on ' + socksServer.host + ':' + socksServer.port);
-            })
-            .useAuth(socks.auth.None());
 
+        // GlobalProxy.setConfig({
+        //     http: `http://socks=${socksServer.host}:${socksServer.port}`,
+        // // https: "https://localhost:1080",
+        // });
+        // GlobalProxy.start();
 
-    // }).connect(sshServer);
-
-
-
-    // conn.on('close', () => {
-    //     console.log('clooooooooooooooooooooooooooooooooooooooooosed');
-    // });
-    // conn.on('error', () => {
-    //     console.log('errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr');
-    // });
-    // conn.on('end', () => {
-    //     console.log('enddddddddddddddddddddddddddddddddddddddddddddd');
-    // });
-} catch (ex2) {
-    console.log('exceeeeeeeeeeeeeeeeption1', ex2);
+        // globalTunnel.initialize({
+        //     host:  `socks=${socksServer.host}`,
+        //     port: socksServer.port,
+        //     // proxyAuth: 'userId:password', // optional authentication
+        //     sockets: 50 // optional pool size for each http and https
+        //   });
+    } 
+    
 }
+ 
+const sshProxy = new SSHProxy()
+sshProxy.start();
 
-process.on('uncaughtException', function (error) { })
+module.exports = sshProxy;
+
+process.on('uncaughtException', function (error) { 
+
+    console.log('uncaughtException' , error);
+})
 
